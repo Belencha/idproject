7489b18294c3e4b7bec1f1ff5fc2324773cec13f
Belen Moreno <belenmore@gmail.com>
2021-02-13 23:30:21 +0100

    Create the first component and show it on the app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyFirstComponentComponent } from './components/my-first-component/my-first-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstComponentComponent
   ],
   imports: [
     BrowserModule
