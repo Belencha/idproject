64f6cf28d43e80b1799d5f5f4559dccb4f1ee29a
Belen Moreno <belenmore@gmail.com>
2021-02-14 01:34:35 +0100

    Practice double binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MyFirstComponentComponent } from './components/my-first-component/my-first-component.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { MyFirstComponentComponent } from './components/my-first-component/my-fi
     MyFirstComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
