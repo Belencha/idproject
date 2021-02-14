405d67f96240304cc11a6ca5ac73a95a268dd517
Belen Moreno <belenmore@gmail.com>
2021-02-14 21:41:05 +0100

    Add button to interruptor

--- src/app/utils/interruptor/interruptor.component.ts
+++ src/app/utils/interruptor/interruptor.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InterruptorComponent implements OnInit {
 
+  activo = true;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  intercambiar() {
+    this.activo = !this.activo;
   }
 
 }
